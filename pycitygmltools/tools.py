65b72708e947d06831e71c7d28ab993ddd0f0274
comtois-etienne <comtois.etienne@outlook.com>
2024-10-17 20:03:58 -0400

    private functions

--- pycitygmltools/tools.py
+++ pycitygmltools/tools.py
@@ -1,11 +1,11 @@
 import subprocess
 
 
-def exec():
+def __exec():
     from . import __executable
     return __executable
 
-def run(command: str, verbose: bool = False):
+def __run(command: str, verbose: bool = False):
     """
     Run a command in a subprocess
     """
@@ -22,50 +22,50 @@ def help():
     """
     ./citygml-tools --help
     """
-    command = f"{exec()} --help"
-    run(command, verbose=True)
+    command = f"{__exec()} --help"
+    __run(command, verbose=True)
 
 def version():
     """
     ./citygml-tools --version
     """
-    command = f"{exec()} --version"
-    run(command, verbose=True)
+    command = f"{__exec()} --version"
+    __run(command, verbose=True)
 
 def stats(citygml_path: str):
     """
     ./citygml-tools stats file.gml
     """
-    command = f"{exec()} stats {citygml_path}"
-    run(command, verbose=True)
+    command = f"{__exec()} stats {citygml_path}"
+    __run(command, verbose=True)
 
 def validate(citygml_path: str, verbose: bool = False):
     """
     ./citygml-tools validate file.gml
     """
-    command = f"{exec()} validate {citygml_path}"
-    run(command, verbose)
+    command = f"{__exec()} validate {citygml_path}"
+    __run(command, verbose)
 
 def apply_xslt(citygml_path: str, xslt_path: str, verbose: bool = False):
     """
     ./citygml-tools apply-xslt file.gml stylesheet.xsl
     """
-    command = f"{exec()} apply-xslt {citygml_path} {xslt_path}"
-    run(command, verbose)
+    command = f"{__exec()} apply-xslt {citygml_path} {xslt_path}"
+    __run(command, verbose)
 
 def change_height(citygml_path: str, offset: float, verbose: bool = False):
     """
     ./citygml-tools change-height file.gml 10
     """
-    command = f"{exec()} change-height {citygml_path} {offset}"
-    run(command, verbose)
+    command = f"{__exec()} change-height {citygml_path} {offset}"
+    __run(command, verbose)
 
 def remove_appereance(citygml_path: str, verbose: bool = False):
     """
     ./citygml-tools remove-apps file.gml
     """
-    command = f"{exec()} remove-apps {citygml_path}"
-    run(command, verbose)
+    command = f"{__exec()} remove-apps {citygml_path}"
+    __run(command, verbose)
 
 def to_local_appearances(citygml_path: str):
     """
@@ -78,8 +78,8 @@ def clip_textures(citygml_path: str, verbose: bool = False):
     """
     ./citygml-tools clip-textures file.gml
     """
-    command = f"{exec()} clip-textures {citygml_path}"
-    run(command, verbose)
+    command = f"{__exec()} clip-textures {citygml_path}"
+    __run(command, verbose)
 
 def subset():
     # todo: implement
@@ -89,22 +89,22 @@ def filter_lods(citygml_path: str, lod: str, verbose: bool = False):
     """
     ./citygml-tools filter-lods file.gml 2
     """
-    command = f"{exec()} filter-lods {citygml_path} {lod}"
-    run(command, verbose)
+    command = f"{__exec()} filter-lods {citygml_path} {lod}"
+    __run(command, verbose)
 
 def reproject(citygml_path: str, epsg: int, verbose: bool = False):
     """
     ./citygml-tools reproject file.gml 4326
     """
-    command = f"{exec()} reproject {citygml_path} {epsg}"
-    run(command, verbose)
+    command = f"{__exec()} reproject {citygml_path} {epsg}"
+    __run(command, verbose)
 
 def json_to_gml(cityjson_path: str, verbose: bool = False):
     """
     ./citygml-tools from-cityjson file.city.json
     """
-    command = f"{exec()} from-cityjson {cityjson_path}"
-    run(command, verbose)
+    command = f"{__exec()} from-cityjson {cityjson_path}"
+    __run(command, verbose)
 
 def gml_to_json(citygml_path: str | list, verbose: bool = False):
     """
@@ -112,12 +112,12 @@ def gml_to_json(citygml_path: str | list, verbose: bool = False):
     """
     if isinstance(citygml_path, list):
         citygml_path = list_to_str(citygml_path)
-    command = f"{exec()} to-cityjson {citygml_path}"
-    run(command, verbose)
+    command = f"{__exec()} to-cityjson {citygml_path}"
+    __run(command, verbose)
 
 def upgrade_3(citygml_path: str, verbose: bool = False):
     """
     ./citygml-tools upgrade-3 file.gml
     """
-    command = f"{exec()} upgrade-3 {citygml_path}"
-    run(command, verbose)
+    command = f"{__exec()} upgrade-3 {citygml_path}"
+    __run(command, verbose)
